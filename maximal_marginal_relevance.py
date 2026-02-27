193f4ff6a42ccde733d48a3920e2647b465327d4
tanyabansal1404 <tanyabansal1404@gmail.com>
2026-02-27 19:40:49 +0530

    Maximal Marginal Relevance

---
+++ maximal_marginal_relevance.py
@@ -0,0 +1,33 @@
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+docs = [
+    Document(page_content="LangChain makes it easy to work with LLMs."),
+    Document(page_content="LangChain is used to build LLM based applications."),
+    Document(page_content="Chroma is used to store and search document embeddings."),
+    Document(page_content="Embeddings are vector representations of text."),
+    Document(page_content="MMR helps you get diverse results when doing similarity search."),
+    Document(page_content="LangChain supports Chroma, FAISS, Pinecone, and more.")
+]
+
+embedding_model = OpenAIEmbeddings()
+
+vector_store = FAISS.from_documents(
+    documents=docs,
+    embedding=embedding_model
+)
+
+retriever = vector_store.as_retriever(
+    search_type="mmr",
+    search_kwargs={"k": 3, "lambda_mult": 0.5}
+)
+
+query = "What is LangChain?"
+
+results = retriever.invoke(query)
+
+print(results)
